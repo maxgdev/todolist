1a74667be509f29b71dbaf8616ded26b01dc35e6
maxgdev <max@apphilia.net>
2019-05-13 13:36:08 +0100

    It should have a way to change a todo

--- todos.js
+++ todos.js
@@ -8,3 +8,8 @@ var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
 // It should have a way to add new todos
 todos.push("item 5");
 console.log ("My Todos: ",todos);
+
+// It should have a way to change a todo
+var index = 1; // index variable to indicate which todo  we want changed
+todos[index] = "Changed item";
+console.log ("My Todos: ",todos);
