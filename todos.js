384cb8f428f06ee93766b0cd16521201417daee5
maxgdev <max@apphilia.net>
2019-05-13 13:31:40 +0100

    It should have a way to add todos

--- todos.js
+++ todos.js
@@ -1,4 +1,10 @@
 // It should have a place to store todos
 // Using arrays
 var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+// It should have a way to display todos
+// console.log ("My Todos: ",todos);
+
+// It should have a way to add new todos
+todos.push("item 5");
 console.log ("My Todos: ",todos);
