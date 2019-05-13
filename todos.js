f31743d49e06744fbb78a90448b16dbcfc826a45
maxgdev <max@apphilia.net>
2019-05-13 14:25:21 +0100

    fixed displayTodos instantiation

--- todos.js
+++ todos.js
@@ -6,23 +6,23 @@ var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
 function displayTodos(){
     console.log ("My Todos: ",todos);
 }
-displayTodos;
+displayTodos();
 
 // It should have a way to add new todos
 function addTodo(todo){
     todos.push(todo);
-    displayTodos;
+    displayTodos();
 }
-addTodo();
-
+addTodo('New item');
+addTodo('Another item');
 
 // It should have a way to change a todo
 var index = 1; // index variable to indicate which todo  we want changed
 todos[index] = "Changed item";
-displayTodos;
+displayTodos();
 
 // It should have a way to delete a todo
 var deleteStart = 0 // where to start delete
 var howManyToDelete = 1 // we are only deleting I item
 todos.splice(deleteStart, howManyToDelete);
-displayTodos;
+displayTodos();
