1ddfc3644a299ba765b45c6a264a58be205a70e1
maxgdev <max@apphilia.net>
2019-05-13 15:46:35 +0100

    Store todos array in TodoList object

--- todos.js
+++ todos.js
@@ -1,6 +1,8 @@
-// It should have a place to store todos
-// Using arrays
-var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
+// It should store the todos array on an object
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3', 'item 4']
+}
+
 
 // It should have a function to display todos
 function displayTodos(){
